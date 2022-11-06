435eeefe750f060ee5f367b159917818b9d90ec9
Stanimir Stoychev <xstukix@gmail.com>
2022-11-06 12:18:09 +0100

    Add inter font from google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
+import { Inter } from '@next/font/google';
+
 import './globals.css';
+
+const inter = Inter();
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
-            {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+        <html lang="en" className={inter.className}>
             <head />
             <body>{children}</body>
         </html>
