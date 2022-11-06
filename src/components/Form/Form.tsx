7a083b3c65a49c88068d4057fc66e573998d2388
Stanimir Stoychev <xstukix@gmail.com>
2022-11-06 21:41:16 +0100

    Update Form
    - Add Button
    - Style Form
    - Update css theme vars

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,5 @@
-import { forwardRef, InputHTMLAttributes, ReactNode } from 'react';
-import clsx from 'clsx';
+import { ButtonHTMLAttributes, DetailedHTMLProps, forwardRef, InputHTMLAttributes, ReactNode } from 'react';
+import clsx, { ClassValue } from 'clsx';
 
 import { BaseComponent } from '@/src/components/types';
 import styles from './Form.module.css';
@@ -33,13 +33,26 @@ const FormInput = forwardRef<HTMLInputElement, FormInputProps>(({ className, lab
 
 FormInput.displayName = 'FormInput';
 
+type FormButtonProps = Omit<
+    DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>,
+    'className'
+> & {
+    fullWidth?: boolean;
+    className?: ClassValue;
+};
+
+function FormButton({ className, fullWidth = false, ...rest }: FormButtonProps) {
+    return <button className={clsx(className, styles['form-button'], { fullWidth })} {...rest} />;
+}
+
 function FormRow({ className, ...rest }: BaseComponent<HTMLDivElement>) {
     return <div className={clsx(className, styles['form-row'])} {...rest} />;
 }
 
 export function Form({ className, ...rest }: BaseComponent<HTMLFormElement>) {
-    return <form className={clsx(className)} {...rest} />;
+    return <form className={clsx(className, styles.form)} {...rest} />;
 }
 
 Form.Input = FormInput;
 Form.Row = FormRow;
+Form.Button = FormButton;
