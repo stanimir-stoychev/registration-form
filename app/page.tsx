782c760ee76d8564eaf009f5d69c01343da620ee
Stanimir Stoychev <xstukix@gmail.com>
2022-11-06 16:54:56 +0100

    Add link to technical challenge pdf

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import clsx from 'clsx';
 
 import styles from './page.module.css';
+import Link from 'next/link';
 
 export default function Home() {
     return (
@@ -43,11 +44,29 @@ export default function Home() {
                         'mb-8',
                         'text-center',
                         'md:text-left',
-                        styles['fade-in'],
+                        styles['slide-in-bottom-subtitle'],
                     )}
                 >
                     by Stanimir Stoychev
                 </p>
+                <ul className={clsx('flex', 'flex-col', styles['fade-in'])}>
+                    <li>
+                        <Link
+                            href="/technical-challenge.pdf"
+                            target="_blank"
+                            className="bg-blue-100 hover:bg-indigo-800 text-gray-800 hover:text-gray-100 font-bold py-2 px-4 rounded inline-flex items-center"
+                        >
+                            <svg
+                                className="fill-current w-4 h-4 mr-2"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                            >
+                                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+                            </svg>
+                            <span>Technical challenge</span>
+                        </Link>
+                    </li>
+                </ul>
             </div>
 
             <div className="w-full xl:w-3/5 py-6 overflow-y-hidden">
