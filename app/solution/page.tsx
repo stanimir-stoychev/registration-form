2bd61027d9cd6ca83c4be77bac9a31214bee6477
Stanimir Stoychev <xstukix@gmail.com>
2022-11-06 21:43:02 +0100

    Add page title to /solution

--- app/solution/page.tsx
+++ app/solution/page.tsx
@@ -1,9 +1,14 @@
-import { Page } from '@/src/components';
+import { Page, Form, Checklist } from '@/src/components';
+import styles from './page.module.css';
 
 export default function Solution() {
     return (
-        <Page className={['pt-24', 'md:pt-48', 'flex', 'flex-wrap', 'flex-col', 'md:flex-row', 'items-center']}>
-            WIP
+        <Page>
+            <div className={styles['page-title-container']}>
+                <h1>Registration</h1>
+            </div>
+            <div className={styles['page-content']}>
+            </div>
         </Page>
     );
 }
